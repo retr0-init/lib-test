f02cd7f781bff0058fed3a528fe2ea949a51551e
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-04-03 21:49:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 import interactions
-# Use the following method to import the internal module in the current same directory
-from . import internal_t
-# Import the os module to get the parent path to the local files
-import os
-# aiofiles module is recommended for file operation
-import aiofiles
-# You can listen to the interactions.py event
-from interactions.api.events import MessageCreate
-# You can create a background task
-from interactions import Task, IntervalTrigger
 
 '''
 Replace the ModuleName with any name you'd like
 '''
-class ModuleName(interactions.Extension):
+class _ModuleName(interactions.Extension):
     pass
